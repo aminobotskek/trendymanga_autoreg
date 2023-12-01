776f23ab139bf13d9a74dbe886ef22c0bdcefa00
Кек <94906343+aminobotskek@users.noreply.github.com>
2023-12-01 19:59:00 +0300

    Add files via upload

---
+++ trendymanga_autoreg.py
@@ -0,0 +1,13 @@
+import trendymanga , temp_mail
+# https://github.com/aminobotskek/trendymanga/blob/main/trendymanga.py
+# https://github.com/aminobotskek/temp_mail/blob/main/temp_mail.py
+from nickname_generator import generate
+password=input("password»")
+while True:
+	client=trendymanga.Client()
+	email=temp_mail.Client().new_email()['email']
+	id=client.register(email=email,password=password,username=generate("en"))['id']
+	print(f"account {email} register . id »{id}")
+	accounts= open("accounts.txt", "a+")
+	accounts.write(f"{email}:{password}\n")
+	accounts.close()
